30db0e453c331a7f753bc52080caa523ee3d66f8
JJFLY <37571306+JJFLYCLOUD@users.noreply.github.com>
2025-07-31 15:45:28 +0800

    Update huaweicloud_ddns.sh

--- huaweicloud_ddns.sh
+++ huaweicloud_ddns.sh
@@ -1,59 +1,61 @@
 #!/bin/bash
 # Copyright 2021 LVCS
-# https://github.com/lllvcs/huaweicloud_ddns
-# https://gitee.com/lvcs/huaweicloud_ddns
 # 在运行此脚本之前，请先在华为云DNS管理控制台内添加对应域名的A记录
 # 并获取对应的 ZONE_ID 和 RECORDSET_ID
 
 # 一般来说用户名和账户名相同
-USERNAME=""
-ACCOUNTNAME=""
-PASSWORD=""
+ACCOUNTNAME=""  #登录时填的用户名
+USERNAME=""        #租户名
+PASSWORD=""  #租户名对应的租户密码
 
 # 对应解析记录的 ZONE_ID 和 RECORDSET_ID
 ZONE_ID=""
 RECORDSET_ID=""
 
-# 从外网API获取ip地址(默认开启1)
-REMOTE_RESOLVE=1
 
-# 从网卡获取ip地址(填写网卡名)
-# 并请根据实际情况填写sed行数
-INTERFACE=""
+# 更新IPv4
+TARGET_IP="$(curl -s ip.sb|grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
+#更新IPv6
+#TARGET_IP="$(curl ipv6.ip.sb)"
 
-# 更新IP
-TARGET_IP=""
 
-# 获取ip地址网址
+# 获取IPv4接口
 # 国内API
-GETIPURL="http://ip.3322.net"
-#GETIPURL="http://members.3322.org/dyndns/getip"
-#GETIPURL="https://myip.ipip.net/"
-# 国外API
-#GETIPURL="http://whatismyip.akamai.com"
-#GETIPURL="https://api.ip.sb/ip"
-#GETIPURL="https://api.myip.la/"
-#GETIPURL="https://v4.ip.zxinc.org/getip"
+#TARGET_IP="$(curl -s ip.3322.net | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
+#TARGET_IP="$(curl -s members.3322.org/dyndns/getip | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
+#TARGET_IP="$(curl -s myip.ipip.net/ | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
+#国外API
+#TARGET_IP="$(curl -s whatismyip.akamai.com | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
+#TARGET_IP="$(curl -s api.myip.la/ | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
+#TARGET_IP="$(curl -s v4.ip.zxinc.org/getip | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)"
 
-# End Point 终端地址 请根据地域选择，默认为北京1
-IAM="iam.myhuaweicloud.com"
+#若有多个IP，可以用 curl --interface 172.16.0.1 …… 来指定本地IP
+
+#获取IPv6接口
+#TARGET_IP="$(curl api6.ipify.org)"
+#TARGET_IP="$(curl 6.ipw.cn)"
+
+
+
+# End Point 终端地址 请根据地域选择，默认为香港
+#IAM="iam.myhuaweicloud.com"
 #IAM="iam.cn-north-4.myhuaweicloud.com"
 #IAM="iam.cn-east-2.myhuaweicloud.com"
 #IAM="iam.cn-east-3.myhuaweicloud.com"
 #IAM="iam.cn-south-1.myhuaweicloud.com"
 #IAM="iam.cn-southwest-2.myhuaweicloud.com"
-#IAM="iam.ap-southeast-1.myhuaweicloud.com"
+IAM="iam.ap-southeast-1.myhuaweicloud.com"
 #IAM="iam.ap-southeast-2.myhuaweicloud.com"
 #IAM="iam.ap-southeast-3.myhuaweicloud.com"
 #IAM="iam.af-south-1.myhuaweicloud.com"
 
-DNS="dns.myhuaweicloud.com"
+#DNS="dns.myhuaweicloud.com"
 #DNS="dns.cn-north-4.myhuaweicloud.com"
 #DNS="dns.cn-east-2.myhuaweicloud.com"
 #DNS="dns.cn-east-3.myhuaweicloud.com"
 #DNS="dns.cn-south-1.myhuaweicloud.com"
 #DNS="dns.cn-southwest-2.myhuaweicloud.com"
-#DNS="dns.ap-southeast-1.myhuaweicloud.com"
+DNS="dns.ap-southeast-1.myhuaweicloud.com"
 #DNS="dns.ap-southeast-2.myhuaweicloud.com"
 #DNS="dns.ap-southeast-3.myhuaweicloud.com"
 #DNS="dns.af-south-1.myhuaweicloud.com"
@@ -87,21 +89,6 @@ TOKEN_X="$(
 
 TOKEN="$(echo $TOKEN_X | awk -F ' ' '{print $2}')"
 
-if [ -z $TARGET_IP ]; then
-    if [ $REMOTE_RESOLVE -eq 1 ]; then
-        if [ $INTERFACE ]; then
-            TARGET_IP=$(curl -s -4 --interface $INTERFACE $GETIPURL | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)
-        else
-            TARGET_IP=$(curl -s -4 $GETIPURL | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)
-        fi
-    else
-        if [ $INTERFACE ]; then
-            TARGET_IP=$(ifconfig $INTERFACE | grep 'inet ' | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)
-        else
-            TARGET_IP=$(ifconfig | grep 'inet ' | grep -oE '([0-9]{1,3}.?){4}' | sed -n 1p)
-        fi
-    fi
-fi
 
 curl -X PUT -L -k -s \
 "https://$DNS/v2/zones/$ZONE_ID/recordsets/$RECORDSET_ID" \
